8674de222e80dcdf3d445d44ab47328579b3d697
007lazar <007lazar@gmail.com>
2026-03-30 21:55:45 +0200

    fix: moved api url to .env file

--- api/eventsApi.ts
+++ api/eventsApi.ts
@@ -1,14 +1,16 @@
-import type { EventModel } from '@/models/event.model';
+/// <reference types="vite/client" />
+import type { EventModel } from '../src/models/event.model';
 import axios from 'axios';
 
-const url = "http://localhost:3000/events";
+const API_URL = import.meta.env.VITE_API_URL;
+const url = `${API_URL}/events`;
 
 export async function fetchEvents(){
     
     try{
         const res = await axios.get<EventModel[]>(url);
         return res.data;
-    }catch (error){
+    } catch (error) {
         throw new Error(`Error: ${error}`);
     }
 }
--- api/gymsApi.ts
+++ api/gymsApi.ts
@@ -1,18 +1,18 @@
-import type { GymModel } from '@/models/gym.model';
+/// <reference types="vite/client" />
+import type { GymModel } from '../src/models/gym.model';
 import axios from 'axios';
 
-const url = "http://localhost:3000/gyms";
+const API_URL = import.meta.env.VITE_API_URL;
+const url = `${API_URL}/gyms`;
 
 export async function fetchGyms() {
     
     try {
         const res = await axios.get<GymModel[]>(url);
         return res.data;
-
-    }catch (error){
+    } catch (error) {
         throw new Error(`Error: ${error}`);
     }
-    
 }
 
 
